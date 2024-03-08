743bf4a5adc3923beff7d0a703bc62f34b3427a3
jpb06 <jpb.06@outlook.com>
2024-03-08 18:33:19 +0100

    feat: extending it with effect

---
+++ src/test/with-string-error.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, vi } from 'vitest';
+import { Effect } from 'effect';
+import { it } from '../extend/it-effect.extend';
+
+describe('yolo bro', () => {
+  it.effect('should effect', () =>
+    Effect.gen(function* (_) {
+      return yield* _(Effect.fail({ _tag: 'Yolo', error: 'Oh no' }));
+    }),
+  );
+
+  it('should not effect', () => {
+    expect(true).toBe(true);
+  });
+});
